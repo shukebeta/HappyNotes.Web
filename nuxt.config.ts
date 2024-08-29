4389040f35aa978237c470076cc0e7955dd46764
DW at xps <weizhong2004@gmail.com>
2024-08-29 20:27:54 +1200

    Rename NUXT_PUBLIC_API_BASE_URL to API_BASE_URL in env files, update nuxt.config.ts
    (81 characters)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import dotenv from 'dotenv'
-dotenv.config()
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   typescript: {
@@ -9,7 +7,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.BASE_URL || 'http://localhost:5013'
+      apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:5013'
     },
     private: {
       // Add private runtime configuration here if needed
